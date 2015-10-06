4b00dad5d4e023f31ece63b116d6d296aaf85d59
unknown <aluno@Dell74-871N0Y1.band.net>
2015-10-05 22:42:34 -0300

    tentativa insana

---
+++ Controller e View/ProjetoTeste/Controllers/GaleriaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoTeste.Controllers
+{
+    public class GaleriaController : Controller
+    {
+        //
+        // GET: /Galeria/
+
+        public ActionResult Galeria()
+        {
+            return View();
+        }
+
+        public ActionResult Readme()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controller e View/ProjetoTeste/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoTeste.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public  ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+        
+        public ActionResult Contato()
+        {
+            return View();
+        }
+    }
+}
